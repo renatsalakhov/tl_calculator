885eb7028f8ee194e0f910e28e3c3b24830163ba
renatsalakhov <35637729+renatsalakhov@users.noreply.github.com>
2020-12-29 14:15:35 +0300

    Initial dist subtree commit

---
+++ dist/precache-manifest.722067d89d585bed233314c824253951.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9312dc09f4c387a4921",
+    "url": "/css/app.a1f0a8c3.css"
+  },
+  {
+    "revision": "5c9394ebdc2341f6e343d90f314212d6",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "1f52a6615e6ea581c4c0b3f63f9000ef",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "33e9b228fe8fae6caeed9314c2fd6e0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9312dc09f4c387a4921",
+    "url": "/js/app.4256d517.js"
+  },
+  {
+    "revision": "947af34cc6466dcd9bfc",
+    "url": "/js/chunk-vendors.c82d1859.js"
+  },
+  {
+    "revision": "d760802630e614fc481195e647cad03c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
