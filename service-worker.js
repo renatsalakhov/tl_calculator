1542142634061dd0a180cae900a5e3b817a7a7ca
renatsalakhov <35637729+renatsalakhov@users.noreply.github.com>
2021-05-11 17:46:03 +0300

    Добавлен калькулятор маржи, небольшие изменения

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8a4e863d67aa15b219df48865291531.js"
+  "/precache-manifest.67656ba585763fce2f6ecb01524d49a2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "calculator"});
